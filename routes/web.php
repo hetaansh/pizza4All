00c5e31da5bfe6334f8aa22b782142b52407eb39
hetaansh <96869880+hetaansh@users.noreply.github.com>
2023-01-17 18:54:34 +0530

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\userController;
+use App\Http\Controllers\productController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/logout', function () {
+    Session::forget('user');
+    return redirect('login');
+});
+
+
+Route::post("/login", [userController::class,'login']);
+Route::post("/register", [userController::class,'register']);
+
+Route::get("/", [productController::class,'index']);
+
+Route::get("detail/{id}", [productController::class,'detail']);
+
+Route::get("search", [productController::class,'search']);
+
+Route::post("add_to_cart", [productController::class,'addToCart']);
+
+Route::get("cartList",[ProductController::class,'cartList']); 
+
+Route::get("removecart/{id}",[ProductController::class,'removeCart']); 
+
+Route::get("ordernow",[ProductController::class,'orderNow']); 
+
+Route::post("orderplace",[ProductController::class,'orderPlace']);
+
+Route::get("myorders",[ProductController::class,'myOrders']);
+
+Route::view("/register",'register');
+
+
+
